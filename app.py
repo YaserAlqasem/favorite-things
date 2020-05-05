8ea5ecff302b6d92b9e532f42ce4ba3c43fc263e
YaserAlqasem <alqasemyaser@gmial.com>
2020-05-04 19:09:04 -0700

    Add CategoriesListResource  Route To app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources.Category import AddCategoryResource
+from resources.Category import AddCategoryResource,CategoriesListResource
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # Route
 api.add_resource(AddCategoryResource, '/AddCategory')
+api.add_resource(CategoriesListResource, '/Categories')
