a09bae55ffa48b3c41ada20c009dd07d3eb4cc20
YaserAlqasem <alqasemyaser@gmial.com>
2020-05-04 17:03:05 -0700

    Add Category Model To models.py

--- models.py
+++ models.py
@@ -6,3 +6,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 ma = Marshmallow()
 db = SQLAlchemy()
+
+
+class Category(db.Model):
+    __tablename__ = 'Category'
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(150), unique=True, nullable=False)
+
+    def __init__(self, Name):
+        self.Name = Name
+
+
+class CategorySchema(ma.Schema):
+    id = fields.Integer()
+    Name = fields.String(required=True)
