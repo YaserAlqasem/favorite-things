81ec3c989ee160a5364ca2941dbdc4b2a52d8938
YaserAlqasem <alqasemyaser@gmial.com>
2020-05-04 23:54:24 -0700

    Add AddItemResource Route To app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Category import AddCategoryResource,CategoriesListResource,EditCategoryResource,DeleteCategoryResource
+from resources.Item import AddItemResource
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -10,3 +11,5 @@ api.add_resource(AddCategoryResource, '/AddCategory')
 api.add_resource(CategoriesListResource, '/Categories')
 api.add_resource(EditCategoryResource, '/EditCategory')
 api.add_resource(DeleteCategoryResource, '/DeleteCategory')
+
+api.add_resource(AddItemResource, '/AddItem')
