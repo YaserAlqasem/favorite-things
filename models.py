e76123733b4ccc772c87d6848751baa11fddec03
YaserAlqasem <alqasemyaser@gmial.com>
2020-05-04 22:12:23 -0700

    Add Item Model To models.py

--- models.py
+++ models.py
@@ -2,7 +2,8 @@ from flask import Flask
 from marshmallow import Schema, fields, pre_load, validate
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.orm import relationship
+import datetime
 
 ma = Marshmallow()
 db = SQLAlchemy()
@@ -12,6 +13,9 @@ class Category(db.Model):
     __tablename__ = 'Category'
     id = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(150), unique=True, nullable=False)
+    
+    
+    item = relationship("Item", back_populates="Category")
 
     def __init__(self, Name):
         self.Name = Name
@@ -20,3 +24,33 @@ class Category(db.Model):
 class CategorySchema(ma.Schema):
     id = fields.Integer()
     Name = fields.String(required=True)
+
+
+class Item(db.Model):
+    __tablename__ = 'Item'
+    id = db.Column(db.Integer, primary_key=True)
+    Title = db.Column(db.String(50), nullable=False)
+    Description = db.Column(db.String(150), nullable=True)
+    Ranking = db.Column(db.Integer, unique=True,nullable=True)
+    Category_id = db.Column(db.Integer, db.ForeignKey(Category.id))
+    CreatedDate = db.Column(db.DateTime, nullable=False)
+    ModifiedDate = db.Column(db.DateTime, nullable=False)
+
+    category = relationship('Category', back_populates="Item")   
+
+    def __init__(self, Title,Description,Ranking,Category_id,CreatedDate,ModifiedDate):
+        self.Title = Title
+        self.Description = Description
+        self.Ranking = Ranking
+        self.Category_id = Category_id
+        self.CreatedDate = CreatedDate
+        self.ModifiedDate = ModifiedDate
+
+class ItemSchema(ma.Schema):
+    id = fields.Integer()
+    Title = fields.String(required=True)
+    Description = fields.String(required=False)
+    Ranking = fields.Integer(required=True)
+    Category_id = fields.Integer(required=True)
+    CreatedDate = fields.DateTime()
+    ModifiedDate = fields.DateTime()
